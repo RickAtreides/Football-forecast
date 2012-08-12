1edbedc30341600b3d6afabd54aab0d322f9a2b6
Rick Atreides BasePC <rick.atreides@gmail.com>
2012-08-12 17:18:22 +0400

    Still work on bets

--- scores.php
+++ scores.php
@@ -30,6 +30,9 @@
 		} else if ($action->action == 'getresults') {
 			$t = getResults($db, $action->season);
 			array_push ( $JSON['results'], [ 'action' => $action->action, 'status' => 0, 'results' => $t ] );
+		} else if ($action->action == 'getchamp') {
+			$t = getChampionat($db, $action->season);
+			array_push ( $JSON['results'], [ 'action' => $action->action, 'status' => 0, 'results' => $t ] );
 		} else if ($action->action == 'getteams') {
 			$t = getTeams($db);
 			array_push ( $JSON['results'], [ 'action' => $action->action, 'status' => 0, 'results' => $t ] );
@@ -91,6 +94,23 @@
 
 	function getResults($db, $season) {
 		$stmt = $db->prepare("SELECT * FROM Championat Where Season = :season AND GoalScored IS NOT NULL;");
+
+		$season = '2012/2013';
+		$stmt->bindValue(':season', $season, SQLITE3_TEXT);
+		$res = $stmt->execute();
+		
+		$resArray = Array();
+		
+		while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+			foreach ($row as $k => $v) { $row[$k] =  iconv('windows-1251', 'utf-8', $v); if ($k == 'GoalScored') { $row[$k] = explode(';', $row[$k]); } }
+			array_push($resArray, $row);
+		}	
+		
+		return $resArray;
+	}
+	
+	function getChampionat($db, $season) {
+		$stmt = $db->prepare("SELECT * FROM Championat Where Season = :season;");
 
 		$season = '2012/2013';
 		$stmt->bindValue(':season', $season, SQLITE3_TEXT);
